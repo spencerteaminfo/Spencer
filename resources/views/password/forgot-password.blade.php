1e18cb508dc62ffd78f29c865a70511a5bbd0eff
Patrik Borza <kepp3.yt@gmail.com>
2026-03-08 12:43:27 +0100

    temporary blade file used for password reset

--- resources/views/password/forgot-password.blade.php
+++ resources/views/password/forgot-password.blade.php
@@ -1,10 +1,17 @@
-@if (session('status'))
-    <div class="alert alert-success">{{ session('status') }}</div>
-@endif
+<!doctype html>
+<html lang="en">
+<x-head title="Reset Password"></x-head>
+<body class="bg-light" data-bs-theme="{{ $activeTheme }}">
+    @if (session('status'))
+        <div class="alert alert-success">{{ session('status') }}</div>
+    @endif
 
-
-<form method="POST" action="{{ route('api.password.email') }}">
-    @csrf
-    <input type="email" name="email" required placeholder="Enter your email">
-    <button type="submit">Send Reset Link</button>
-</form>
+    <div class="card shadow-sm border-0 rounded-4 p-4 mb-4 w-50" style="position:fixed; left: 50%; top: 50%;transform: translate(-50%, -50%);">
+        <h2 class="h3 fw-bold mb-4 text-secondary">Reset password link</h2>
+        <form method="POST" action="{{ route('api.password.email') }}" class="d-flex gap-2 flex-column">
+            @csrf
+            <input type="email" name="email" required placeholder="Enter your email" class="form-control rounded-3">
+            <button type="submit" id="save-changes" class="btn btn-primary w-100 py-2 fw-bold shadow-sm">Send link</button>
+        </form>
+    </div>
+</body>
--- resources/views/password/reset.blade.php
+++ resources/views/password/reset.blade.php
@@ -1,4 +1,4 @@
-<form method="POST" action="{{ route('api.password.update') }}">
+{{-- <form method="POST" action="{{ route('api.password.update') }}">
     @csrf
 
     <input type="hidden" name="token" value="{{ $token }}">
@@ -19,4 +19,30 @@
     </div>
 
     <button type="submit">Update Password</button>
-</form>
+</form> --}}
+
+
+<!doctype html>
+<html lang="en">
+<x-head title="Reset Password"></x-head>
+<body class="bg-light" data-bs-theme="{{ $activeTheme }}">
+    <div class="card shadow-sm border-0 rounded-4 p-4 mb-4 w-50" style="position:fixed; left: 50%; top: 50%;transform: translate(-50%, -50%);">
+        <h2 class="h3 fw-bold mb-4 text-secondary">Reset password</h2>
+        <form method="POST" action="{{ route('api.password.update') }}" class="d-flex flex-column gap-2">
+            @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
+            <div>
+                <input type="email" name="email" value="{{ $email ?? old('email') }}" required class="form-control rounded-3" placeholder="Enter your email">
+            </div>
+            
+            <div>
+                <input type="password" name="password" required class="form-control rounded-3" placeholder="Enter your new password">
+            </div>
+
+            <div>
+                <input type="password" name="password_confirmation"" required class="form-control rounded-3" placeholder="Enter password confirmation">
+            </div>
+            <button type="submit" id="save-changes" class="btn btn-primary w-100 py-2 fw-bold shadow-sm">Update password</button>
+        </form>
+    </div>
+</body>
