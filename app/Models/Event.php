bbb64b7a68c35822611195e450c72c8242e52435
pismeno <krystof.kloz@gmail.com>
2026-04-23 19:30:27 +0200

    added missing imports

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
+use App\Enums\RoleType;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Collection;
 
 /**
  * @property Carbon|null $created_at
